476e80f5efa8e70ea801bbd8b71e20b4d411c82b
Maria <tarasovych.maria@gmail.com>
2022-09-30 00:11:24 +0300

    1 ex fix

--- lab_1.py
+++ lab_1.py
@@ -1,5 +1,5 @@
 sec = int(input('seconds -> '))
-print('h - hours\nm - minutes\ns - seconds\nfull - full time')
+print('h - hours\nm - minutes\ns - seconds')
 op = input('=> ')
 res = ""
 
@@ -14,27 +14,3 @@ if op =='m':
 if op =='s':
     res = (f'{int((86400 - sec))} seconds')
     print(f'res = {res}')
-
-
-if op == 'full':
-    end = 86400
-    between = end-sec
-    print(f'ur time:{int(sec/3600)}: {int(sec%3600/60)}:{int:((sec%3600)%60)}')
-
-
-
-#(f'{int(input(86400 - sec)/60)} minutes')
-
-#(f'{int(86400 - sec)/60} minutes')
-
-
-
-#end = 86400
-#   between = end-sec
-
-#if between > 0:
-#  h = int(between/3600)
-#  m = int(between % 3600/60)
-#  s = int(between%3600%60)
-#print(f'Left time: {h}:{m}:{s}')
-
