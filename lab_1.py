9a1a64476630b2d2c5ab5cf22e8fbddcb1b02d2f
Maria <tarasovych.maria@gmail.com>
2022-09-29 21:38:11 +0300

    first ex

---
+++ lab_1.py
@@ -0,0 +1,34 @@
+sec = int(input('seconds ->'))
+print('h-hours\nm -minutes\ns - seconds\nfull - full time')
+op = input('=>')
+res = ""
+
+if op == 'h':
+    res = (f'{int((86400 - sec)/3600)} hours')
+    print(f'res = {res}')
+elif op =='m':
+    res = (f'{int(86400 - sec)/60} minutes')
+    print(f'res = {res}')
+
+elif op =='s':
+    res = (f'{int(86400 - sec)} seconds')
+    print(f'res = {res}')
+
+if op == 'full':
+    end = 86400
+    between = end-sec
+    print(f'ur time:{int(sec/3600)}: {int(sec%3600/60)}:{int:((sec%3600)%60)}')
+
+
+
+
+
+#end = 86400
+ #   between = end-sec
+
+#if between > 0:
+      #  h = int(between/3600)
+      #  m = int(between % 3600/60)
+      #  s = int(between%3600%60)
+      #print(f'Left time: {h}:{m}:{s}')
+
