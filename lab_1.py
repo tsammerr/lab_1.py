0af2f9eaa7b89659e66abab5ec2dbc52864c5938
Maria <tarasovych.maria@gmail.com>
2022-09-29 22:45:51 +0300

    4 ex ready

--- lab_1.py
+++ lab_1.py
@@ -1,20 +1,19 @@
+g = int(input('file size -> '))
+s = int(input('internet speed -> '))
 
+print('For loading this file u will need:\n1 -- hours\n2 -- minutes\n3 -- seconds')
 
+op = input('-> ')
+res = ""
 
+if op == '1':
+    res = int(((g*8589934592)/s)/3600)
+    print(f'res = {res} hours')
 
+if op == '2':
+    res = int(((g*8589934592)/s)/60)
+    print(f'res = {res} minutes')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-#Користувач вводить з клавіатури розмір файлу в гігабайтах і швидкість інтернет-з’єднання в бітах за секунду.
-#Залежно від вибору користувача, підрахуйте, скільки знадобиться годин, хвилин або секунд для завантаження файлу
+if op == '3':
+    res = int((g*8589934592)/s)
+    print(f'res = {res} seconds')
