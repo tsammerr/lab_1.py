bf9e4b55745fec94f2d53cf6f04e77b755f274a5
Maria <tarasovych.maria@gmail.com>
2022-09-29 22:12:21 +0300

    3 ex fix!

--- lab_1.py
+++ lab_1.py
@@ -3,15 +3,15 @@ c = int(input('cost -> '))
 d = int(input('discount% -> '))
 print('1 -- total cost\n2 -- discounted price of one')
 
-op = input('->')
+op = input('-> ')
 res = ""
 
 
 if op == '1':
-    print('23')
+    res = int(n*(c - (d * c / 100)))
 
 
 if op == '2':
-    res = int(n - (d*c/100))
+    res = int(c - (d*c/100))
 
 print(f'res = {res}')
